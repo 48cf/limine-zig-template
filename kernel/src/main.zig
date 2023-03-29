46a736d26294745c2627b7058d61bb28554ac69d
Gabriel Waksmundzki <czapek1337@gmail.com>
2023-03-29 22:33:48 +0200

    Replace terminal example with drawing a line to the framebuffer

--- kernel/src/main.zig
+++ kernel/src/main.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent. In Zig, `export var` is what we use.
-pub export var terminal_request: limine.TerminalRequest = .{};
+pub export var framebuffer_request: limine.FramebufferRequest = .{};
 
 inline fn done() noreturn {
     while (true) {
@@ -14,15 +14,23 @@ inline fn done() noreturn {
 
 // The following will be our kernel's entry point.
 export fn _start() callconv(.C) noreturn {
-    // Ensure we got a terminal
-    if (terminal_request.response) |terminal_response| {
-        if (terminal_response.terminal_count < 1) {
+    // Ensure we got a framebuffer.
+    if (framebuffer_request.response) |framebuffer_response| {
+        if (framebuffer_response.framebuffer_count < 1) {
             done();
         }
 
-        // We should now be able to call the Limine terminal to print out
-        // a simple "Hello World" to screen.
-        terminal_response.write(null, "Hello World");
+        // Get the first framebuffer's information.
+        const framebuffer = framebuffer_response.framebuffers()[0];
+
+        for (0..100) |i| {
+            // Calculate the pixel offset using the framebuffer information we obtained above.
+            // We skip `i` scanlines (pitch is provided in bytes) and add `i * 4` to skip `i` pixels forward.
+            const pixel_offset = i * framebuffer.pitch + i * 4;
+
+            // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
+            @ptrCast(*u32, @alignCast(4, framebuffer.address + pixel_offset)).* = 0xFFFFFFFF;
+        }
     }
 
     // We're done, just hang...
